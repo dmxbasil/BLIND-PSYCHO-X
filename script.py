c7c448239717eadc2e5faf3546cdf87a86f89b59
BASIL » <77526924+dmxbasil@users.noreply.github.com>
2021-07-31 16:17:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -52,11 +52,11 @@ I'm an advanced filter bot created by @basildmx with many capabilities ❗️
 """
 
 
-    ABOUT_MSG = """❍❍<b>My Name >: 𝗕𝗟𝗜𝗡𝗗»𝗣𝗦𝗬𝗖𝗛𝗢 </b>
+    ABOUT_MSG = """❍❍<b>My Name > 𝗕𝗟𝗜𝗡𝗗»𝗣𝗦𝗬𝗖𝗛𝗢 </b>
 
-❍<b>👿 𝙲𝚛𝚎𝚊𝚝𝚎𝚛:</b> @basildmx    
+❍<b>👿 𝙲𝚛𝚎𝚊𝚝𝚎𝚛:</b> <a href='https://t.me/basildmx'>BASIL</a>
 
-❍<b>⌨︎ 𝙻𝚊𝚗𝚐𝚞𝚊𝚐𝚎:</b> <code>Python3</code>
+❍<b>⁂𝙻𝚊𝚗𝚐𝚞𝚊𝚐𝚎:</b> <code>Python3</code>
 
 ❍<b>⁂ 𝙻𝚒𝚋𝚛𝚊𝚛𝚢 :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 ❍<b>⁂ 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 :</b> <a href='https://t.me/basildmx'>CLICK HERE</a>
